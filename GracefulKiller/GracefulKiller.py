94484ae7bdb70eabdaf72c38b1bd41ff067a0c00
williams824 <produqzai824@gmail.com>
2022-04-25 18:39:06 +0800

    There is no SIGHUP signal under windows

--- GracefulKiller/GracefulKiller.py
+++ GracefulKiller/GracefulKiller.py
@@ -7,16 +7,20 @@ from threading import Event
 class KillerEvent(Event):
     def __nonzero__(self):
         return self.is_set()
-    
+
     def __len__(self):
         return self.is_set()
+
 
 # process SIGTERM and SIGINT signals gracefully
 class GracefulKiller:
     def __init__(self):
         self.kill_now = KillerEvent()
         for sig in ('TERM', 'HUP', 'INT'):
-            signal.signal(getattr(signal, 'SIG' + sig), self.exit_gracefully)
+            signalnum = getattr(signal, 'SIG' + sig, None)
+            if signalnum is None:
+                continue
+            signal.signal(signalnum, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
         self.kill_now.set()
