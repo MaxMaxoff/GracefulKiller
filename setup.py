a1663b68fea9d69227ca8f44b29317f29cae0be0
Maxim Toropov <maxim@toropov.club>
2022-04-25 14:09:15 +0300

    PR from williams824

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name = 'GracefulKiller',
   packages = ['GracefulKiller'],
-  version = '0.2',
+  version = '0.3',
   license = 'MIT',
   description = "module for process SIGTERM, SIGHUP and SIGINT signals gracefully",
   author = 'Maxim Toropov',
